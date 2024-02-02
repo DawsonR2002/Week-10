ea0592575e0a5cddcc680f728a4c0b6080d433fd
DawsonR2002 <dawsonring2020@gmail.com>
2024-02-02 16:11:13 -0600

    week 10

---
+++ src/main/java/projects/dao/ProjectDao.java
@@ -0,0 +1,216 @@
+package projects.dao;
+
+import java.math.*;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import projects.*;
+import projects.entity.Project;
+import projects.entity.Step;
+import projects.entity.Category;
+import projects.entity.Material;
+import projects.exception.DbException;
+@SuppressWarnings("unused")
+public class ProjectDao extends DaoBase {
+	private static final String CATEGORY_TABLE = "category";
+	private static final String MATERIAL_TABLE = "material";
+	private static final String PROJECT_TABLE = "project";
+	private static final String PROJECT_CATEGORY = "project_category";
+	private static final String STEP_TABLE = "step";
+	
+	
+	
+	public Project insertProject(Project project) {
+		
+		
+		String sql = "" + "INSERT INTO " + PROJECT_TABLE + " " 
+				//			good													good
+						+ "(project_name, estimated_hours, actual_hours, difficulty, notes) "
+						+ "VALUES "
+						+ "(?, ?, ?, ?, ?)";
+		//System.out.println("test");
+		try(Connection conn = DbConnection.getConnection()) {
+			startTransaction(conn);
+			
+			try(PreparedStatement stmt = conn.prepareStatement(sql)){
+				setParameter(stmt, 1, project.getProjectName(), String.class);
+				setParameter(stmt, 2, project.getEstimatedHours(), BigDecimal.class);
+				setParameter(stmt, 3, project.getActualHours(), BigDecimal.class);
+				setParameter(stmt, 4, project.getDifficulty(), Integer.class);
+				setParameter(stmt, 5, project.getNotes(), String.class);
+				
+				stmt.executeUpdate();
+				
+				
+				
+				Integer projectID = getLastInsertId(conn,PROJECT_TABLE);
+				commitTransaction(conn);
+				project.setProjectId(projectID);
+				return project;
+			}
+			catch(Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+		}
+		catch(Exception e) {
+			throw new DbException(e);
+		}
+	}
+
+	public void executeBatch(List<String> sqlBatch) {
+    try (Connection conn = DbConnection.getConnection()) {
+      startTransaction(conn);
+
+      try (Statement stmt = conn.createStatement()) {
+        /*
+         * Add each SQL line to the Statement so they can be executed as a
+         * batch.
+         */
+        for (String sql : sqlBatch) {
+          stmt.addBatch(sql);
+        }
+
+        stmt.executeBatch();
+        commitTransaction(conn);
+
+      } catch (Exception e) {
+        rollbackTransaction(conn);
+        throw new DbException(e);
+      }
+    } catch (SQLException e) {
+      throw new DbException(e);
+    }
+  }
+	
+	public List<Project> fetchAllProjects() {
+		String sql = "SELECT * FROM " + PROJECT_TABLE + " ORDER BY project_name";
+		try(Connection conn = DbConnection.getConnection()){
+			startTransaction(conn);
+			
+			try(PreparedStatement stmt = conn.prepareStatement(sql)){
+				try(ResultSet rs = stmt.executeQuery()){
+					List<Project> projects = new LinkedList<>();
+					
+					while(rs.next()) {
+						projects.add(extract(rs, Project.class));
+					}
+					return projects;
+				}
+			}
+			catch(Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e);
+		}
+	}
+	
+	public Optional<Project> fetchProjectById(Integer projectId){
+		String sql = "SELECT * FROM " + PROJECT_TABLE + " WHERE project_id = ?";
+		try(Connection conn = DbConnection.getConnection()){
+			startTransaction(conn);
+			try {
+				Project project = null;
+				
+				try(PreparedStatement stmt = conn.prepareStatement(sql)){
+					setParameter(stmt, 1, projectId, Integer.class);
+					
+					try(ResultSet rs = stmt.executeQuery()){
+						if(rs.next()) {
+							project = extract(rs, Project.class);
+						}
+					}
+				}
+				
+				
+				if(Objects.nonNull(project)) {
+					project.getMaterials().addAll(fetchMaterialsForProject(conn, projectId));
+					project.getSteps().addAll(fetchStepsForProject(conn, projectId));
+					project.getCategories().addAll(fetchCategoriesForProject(conn, projectId));
+				}
+				
+				commitTransaction(conn);
+				return Optional.ofNullable(project);
+			}
+			
+			catch(Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+			
+		
+		}
+		catch(SQLException e) {
+			throw new DbException(e);
+		}
+
+	}
+	
+	
+	private List<Category> fetchCategoriesForProject(Connection conn, Integer projectId) throws SQLException {
+		String sql = "" + "SELECT c.* FROM " + CATEGORY_TABLE + " c " + "JOIN " + PROJECT_CATEGORY + " pc USING (category_id) " + "WHERE project_id = ?";
+		try(PreparedStatement stmt = conn.prepareStatement(sql)){
+			setParameter(stmt, 1, projectId, Integer.class);
+			
+			try(ResultSet rs = stmt.executeQuery()){
+				List<Category> categories = new LinkedList<>();
+				while(rs.next()) {
+					categories.add(extract(rs, Category.class));
+				}
+				return categories;
+			}
+		}
+	}
+	
+	private List<Step> fetchStepsForProject(Connection conn, Integer projectId) throws SQLException {
+		String sql = "SELECT * FROM " + STEP_TABLE + " WHERE project_id = ?";
+		
+		
+			
+			try(PreparedStatement stmt = conn.prepareStatement(sql)){
+				setParameter(stmt, 1, projectId, Integer.class);
+				
+				try(ResultSet rs = stmt.executeQuery()){
+					List<Step> steps = new LinkedList<>();
+					while(rs.next()) {
+						
+
+						steps.add(extract(rs, Step.class));
+						
+						
+					}
+					
+					return steps;
+				}
+			}
+	}
+	
+	private List<Material> fetchMaterialsForProject(Connection conn, Integer projectId) throws SQLException {
+		String sql = "SELECT * FROM " + MATERIAL_TABLE + " WHERE project_id = ?";
+		
+		
+			
+			try(PreparedStatement stmt = conn.prepareStatement(sql)){
+				setParameter(stmt, 1, projectId, Integer.class);
+				
+				try(ResultSet rs = stmt.executeQuery()){
+					List<Material> materials = new LinkedList<>();
+					while(rs.next()) {
+						
+
+						materials.add(extract(rs, Material.class));
+						
+						
+					}
+					
+					return materials;
+				}
+			}
+	}
+}
